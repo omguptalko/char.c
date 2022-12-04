8ad785de1a542c62c828c1e3a9f5922c3de76048
omguptalko <119674842+omguptalko@users.noreply.github.com>
2022-12-04 12:29:34 +0530

    Add files via upload

---
+++ char.c
@@ -0,0 +1,10 @@
+/* program to display the ASCII valve of  character */
+#include<stdio.h>
+int main()
+{
+ char r ;
+printf("enter the value of character\n") ;
+scanf("%c",&r) ; 
+printf("Character entered by user %c = %d\n", r,r ) ;
+return 0 ;
+}
